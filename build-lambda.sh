ddcf00a07fd052ffd38bdd53edd5afacf10f83d2
Brian Munroe <bmunroe@zappos.com>
2018-10-06 08:34:03 -0700

    more updating

--- build-lambda.sh
+++ build-lambda.sh
@@ -10,7 +10,13 @@ echo "Rebuilding Deployment Package...";
 cd ${lambda_function_VIRTUALENV}/lib/python3.7/site-packages
 zip -r9 ${BUILD_DIR}/lambda_function.zip *
 cd ${BUILD_DIR};
-zip -d lambda_function.zip "pip/*";
-zip -d lambda_function.zip "setuptools/*";
+
+to_remove="astroid idna isort lazy-object-proxy mccabe pylint six wrapt pip setuptools";
+
+# Remove unnecessary dev libraries from lambda upload package 
+for i in ${to_remove}; do
+    zip -d lambda_function.zip "${i}/*";
+done 
+
 zip -g lambda_function.zip lambda_function.py;
 zip -g lambda_function.zip local_config.py;
